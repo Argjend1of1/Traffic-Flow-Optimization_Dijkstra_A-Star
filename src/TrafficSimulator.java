3ba8b2cfa681f2508ba7519a34bd5ac4ad70e657
Brikenda-Zogaj <116032682+Brikenda-Zogaj@users.noreply.github.com>
2024-12-18 22:21:24 +0100

    Added functionality to simulate real-time traffic updates in the graph

--- src/TrafficSimulator.java
+++ src/TrafficSimulator.java
@@ -1,8 +1,19 @@
 import java.util.*;
 public class TrafficSimulator {
     public static void simulateTrafficUpdates(Graph graph) {
-
-
-
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                Random rand = new Random();
+                for (String node : graph.getNodes()) {
+                    for (Map.Entry<String, Double> entry : graph.getNeighbors(node).entrySet()) {
+                        double newWeight = 1 + rand.nextDouble() * 10;
+                        graph.updateTraffic(node, entry.getKey(), newWeight);
+                    }
+                }
+                System.out.println("Traffic updated.");
+            }
+        }, 0, 50000);
     }
 }
