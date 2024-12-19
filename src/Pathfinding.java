a7956ad7b29ce9b9985cb5a11120ff145d92170d
Brikenda-Zogaj <116032682+Brikenda-Zogaj@users.noreply.github.com>
2024-12-19 15:07:34 +0100

    Heuristic calculation method for A* algorithm using Euclidean distance

--- src/Pathfinding.java
+++ src/Pathfinding.java
@@ -1,2 +1,9 @@
+import java.util.Map;
+
 public class Pathfinding {
+    static double calculateHeuristic(String node, String goal, Map<String, int[]> coordinates) {
+        int[] currentCoords = coordinates.get(node);
+        int[] goalCoords = coordinates.get(goal);
+        return Math.sqrt(Math.pow(goalCoords[0] - currentCoords[0], 2) + Math.pow(goalCoords[1] - currentCoords[1], 2));
+    }
 }
